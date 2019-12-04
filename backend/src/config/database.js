bb43eb9982c51f2a7432eac66f4991e77565d4bf
Leonardo da Silva <dasilva.leonardo@gmail.com>
2019-12-04 13:23:36 -0300

    Desafio 2

---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '10.0.75.1',
+  username: 'postgres',
+  password: 'postgres',
+  database: 'gympoint',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
